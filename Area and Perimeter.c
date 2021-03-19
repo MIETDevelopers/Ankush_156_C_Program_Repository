cc525587c5540ec90f68b409d9dc312ed95be371
warriormighty <75359020+warriormighty@users.noreply.github.com>
2021-03-19 18:51:30 +0530

    To find area and perimeter of following figures

---
+++ Area and Perimeter.c
@@ -0,0 +1,22 @@
+// Program to find out the area and perimeter of square, rectangle and circle //
+
+/* Name -  Ankush kumar
+   class - cse_A2_156
+   */
+
+#include <stdio.h>	//PREPROSESSIVE DIRECTIVE TO INCLUDE STANDARD INPUT OUTPUT HEADER FILE
+
+int main()	//STARTING OF MAIN PROGRAM
+{
+	float radiusofcircle, sideofsquare, lengthofrectangle, bredthofrectangle;	/*DECLARING
+	VARIABLES NAMED "radiusofcircle", "sideofsquare", "lengthofrectangle", "bredthofrectangle" */
+	scanf("%f %f %f %f", &radiusofcircle, &sideofsquare, &lengthofrectangle, &bredthofrectangle);
+	/* READ radiusofcircle, sideofsquare, lengthofractangle, bredthofrectangle */
+	printf("\nArea of Circle =%f ",3.14*radiusofcircle*radiusofcircle);	//PRINT AREA OF CIRCLE
+	printf("\nPerimeter of Circle =%f ",2*3.14*radiusofcircle);	//PRINT PERIMETER OF CIRCLE
+	printf("\nArea of Square = %f",sideofsquare*sideofsquare);	//PRINT AREA OF SQUARE
+	printf("\nPerimeter of Square =%f",4*sideofsquare);	//PRINT PERIMETER OF SQUARE
+	printf("\nArea of Rectangle =%f",lengthofrectangle*bredthofrectangle);	//PRINT AREA OF RECTANGLE
+	printf("\nPerimeter of Rectangle =%f",2*(lengthofrectangle+bredthofrectangle));	//PRINT PERIMETER OF RECTANGLE
+	return 0;	//RETURN STATEMENT
+}
