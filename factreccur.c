fceefc61373164dc130480243476dd912d5b2d06
warriormighty <75359020+warriormighty@users.noreply.github.com>
2021-03-31 14:31:33 +0530

    Factorial calculation using recurssion function

---
+++ factreccur.c
@@ -0,0 +1,22 @@
+// Program to demonstrate factorial calculation using recursive function//
+
+/* Name -  Ankush kumar
+   class - cse_A2_156
+   */
+
+#include<stdio.h>
+long int multiplyNumbers(int n);
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+
+long int multiplyNumbers(int n) {
+    if (n>=1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
+}
